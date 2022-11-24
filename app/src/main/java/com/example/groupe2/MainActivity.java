372d8cbf04dc2cd9ab2403f809f44a4dcb131548
natzan <nalancelot@stpbb.org>
2022-11-24 12:01:45 +0100

    hello?

--- app/src/main/java/com/example/groupe2/MainActivity.java
+++ app/src/main/java/com/example/groupe2/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-sa marche j
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
